2ee793e2c7494a92dc47d577817687022fa6f857
muxazzz <60567035+muxazzz@users.noreply.github.com>
2020-02-13 22:10:20 +0300

    Add files via upload

---
+++ js/script-a6.js
@@ -0,0 +1,24 @@
+
+const maxAllowedChecks = 2;
+
+function init () {
+	$("input[type=checkbox]").change(trackChecks);
+	$("input[type=radio]").change(trackRadios);
+	console.log("скрипт подгрузился");
+	  trackChecks();
+  trackRadios();
+}
+
+function trackChecks () {
+	let checkCount = $("input[type=checkbox]:checked").length;
+	console.log(`Выбрано флажков: ${checkCount}`);
+	$("input[type=checkbox]:not(:checked)").prop("disabled", checkCount >= maxAllowedChecks);
+}
+
+function trackRadios () {
+	let checkRadios = $("input[type=radio]:checked").length;
+	console.log(`Выбрано кнопок: ${checkRadios}`);
+	$("input[type=radio]").prop("disabled",false)
+}
+
+$(document).ready(init);
